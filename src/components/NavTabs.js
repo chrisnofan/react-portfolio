e4e9d9bdd83ffe60991fc7abfac805eb98943244
chrisnofan <chrisnofan@gmail.com>
2023-03-07 00:44:21 +0100

    created the file structure, added the home page component

---
+++ src/components/NavTabs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      
+    </ul>
+  );
+}
+
+export default NavTabs;
